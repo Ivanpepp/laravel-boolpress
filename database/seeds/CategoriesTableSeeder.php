0f3acfe84f0c4f7a3e95ec95148a28f605453dcb
Ivanpepp <nastroivan3@gmail.com>
2021-11-18 15:15:49 +0100

    added independent category table and seeded

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategoriesTableSeeder extends Seeder
+{
+
+    
+
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+         $categoryNames = ['HTML','PHP','JS','MySQL','LARAVEL','VueJS'];
+         foreach($categoryNames as $name){
+            $category = new Category();
+            $category->name = $name;
+
+            $category->slug = Str::slug($name,'-');
+
+            $category->save();
+         }
+    }
+}
